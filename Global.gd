6e5234a0a6d9ddb8dbe2def07fb3bb878b1aa534
Torben Lochow <t.lochow@gmx.de>
2020-07-16 20:33:02 +0200

    Color Function

--- Global.gd
+++ Global.gd
@@ -29,6 +29,9 @@ var CherryColor = Color(1.0, 0.0, 0.0)
 var ExplosionColor = Color(0.933, 0.0, 0.0)
 var GhostColor = Color(0.0, 0.882, 1.0)
 
+func _ready():
+	LoadColors()
+
 func SetSnakeSpeed(speed):
 	SnakeSpeed = speed
 	UpdateDelay = 60.0 / SnakeSpeed
@@ -73,4 +76,29 @@ func SaveShader():
 	var result = config.load("user://settings.cfg")
 	config.set_value("settings", "glow_shader_active", GlowShaderActive)
 	config.set_value("settings", "retro_shader_active", RetroShaderActive)
+	config.save("user://settings.cfg")
+
+func LoadColors():
+	LevelColor = Color(1.0, 0.0, 0.471)
+	SnakeColor = Color(0.0, 0.667, 0.043)
+	CherryColor = Color(1.0, 0.0, 0.0)
+	ExplosionColor = Color(0.933, 0.0, 0.0)
+	GhostColor = Color(0.0, 0.882, 1.0)
+	var config = ConfigFile.new()
+	var result = config.load("user://settings.cfg")
+	if result == OK:
+		LevelColor = config.get_value("colors", "level_color", Color(1.0, 0.0, 0.471))
+		SnakeColor = config.get_value("colors", "snake_color", Color(0.0, 0.667, 0.043))
+		CherryColor = config.get_value("colors", "cherry_color", Color(1.0, 0.0, 0.0))
+		ExplosionColor = config.get_value("colors", "explosion_color", Color(0.933, 0.0, 0.0))
+		GhostColor = config.get_value("colors", "ghost_color", Color(0.0, 0.882, 1.0))
+
+func SaveColors():
+	var config = ConfigFile.new()
+	var result = config.load("user://settings.cfg")
+	config.set_value("colors", "level_color", LevelColor)
+	config.set_value("colors", "snake_color", SnakeColor)
+	config.set_value("colors", "cherry_color", CherryColor)
+	config.set_value("colors", "explosion_color", ExplosionColor)
+	config.set_value("colors", "ghost_color", GhostColor)
 	config.save("user://settings.cfg")
--- scenes/MainMenu.gd
+++ scenes/MainMenu.gd
@@ -8,6 +8,7 @@ func _ready():
 	Global.LoadLevel()
 	SetLevelButtonsEnabled()
 	SetMuteButtonText()
+	SetColorSliderValues()
 
 func _on_StartButton_pressed():
 	$ButtonPressed.play()
@@ -118,7 +119,108 @@ func _on_SettingsButton_pressed():
 	$Main.visible = false
 	$Settings.visible = true
 
+func _on_ColorsButton_pressed():
+	$ButtonPressed.play()
+	$Settings/Colors.visible = true
+	$Settings/Main.visible = false
+	
+func _on_ColorBackButton_pressed():
+	$ButtonPressed.play()
+	$Settings/Colors.visible = false
+	$Settings/Main.visible = true
+
 func _on_SettingsBackButton_pressed():
 	$ButtonPressed.play()
+	Global.SaveColors()
 	$Main.visible = true
 	$Settings.visible = false
+
+func UpdateColors():
+	# Level
+	SetLabelColor(get_tree().root)
+	$HowTo/Sprites/TileMap.modulate = Global.LevelColor
+	
+	# Snake
+	$Settings/Colors/Snake/Sprite1.modulate = Global.SnakeColor
+	$Settings/Colors/Snake/Sprite2.modulate = Global.SnakeColor
+	$Settings/Colors/Snake/Sprite3.modulate = Global.SnakeColor
+	$Settings/Colors/Snake/Sprite4.modulate = Global.SnakeColor
+	$HowTo/Sprites/Sprite2.modulate = Global.SnakeColor
+	$HowTo/Sprites/Sprite3.modulate = Global.SnakeColor
+	$HowTo/Sprites/Sprite4.modulate = Global.SnakeColor
+	$HowTo/Sprites/Sprite5.modulate = Global.SnakeColor
+	$HowTo/Sprites/Sprite6.modulate = Global.SnakeColor
+	$HowTo/Sprites/Sprite7.modulate = Global.SnakeColor
+	$HowTo/Sprites/Sprite8.modulate = Global.SnakeColor
+	$HowTo/Sprites/Sprite9.modulate = Global.SnakeColor
+	$HowTo/Sprites/Sprite11.modulate = Global.SnakeColor
+	$HowTo/Sprites/Sprite12.modulate = Global.SnakeColor
+	$HowTo/Sprites/Sprite13.modulate = Global.SnakeColor
+	$HowTo/Sprites/Sprite14.modulate = Global.SnakeColor
+	$HowTo/Sprites/Sprite15.modulate = Global.SnakeColor
+	$HowTo/Sprites/Sprite16.modulate = Global.SnakeColor
+	$HowTo/Sprites/Sprite17.modulate = Global.SnakeColor
+	$HowTo/Sprites/Sprite18.modulate = Global.SnakeColor
+	$HowTo/Sprites/Sprite19.modulate = Global.SnakeColor
+	$HowTo/Sprites/Sprite20.modulate = Global.SnakeColor
+	$HowTo/Sprites/Sprite21.modulate = Global.SnakeColor
+	$HowTo/Sprites/Sprite22.modulate = Global.SnakeColor
+	$HowTo/Sprites/Sprite23.modulate = Global.SnakeColor
+	
+	# Cherry
+	$Settings/Colors/Cherry/Sprite1.modulate = Global.CherryColor
+	$HowTo/Sprites/Sprite.modulate = Global.CherryColor
+	
+	# Ghost
+	$Settings/Colors/Ghost/Sprite1.modulate = Global.GhostColor
+	$HowTo/Sprites/Sprite10.modulate = Global.GhostColor
+	$HowTo/Sprites/Sprite24.modulate = Global.GhostColor
+	$HowTo/Sprites/Sprite25.modulate = Global.GhostColor
+	$HowTo/Sprites/Sprite26.modulate = Global.GhostColor
+	$HowTo/Sprites/Sprite27.modulate = Global.GhostColor
+	
+	# Explosion
+	$Settings/Colors/Explosion/Sprite1.modulate = Global.ExplosionColor
+	$HowTo/Sprites/Sprite29.modulate = Global.ExplosionColor
+
+func SetLabelColor(node):
+	if node is Label:
+		node.modulate = Global.LevelColor
+	var children = node.get_children()
+	for child in children:
+		SetLabelColor(child)
+
+func SetColorSliderValues():
+	$Settings/Colors/Level/LevelColorSlider.value = Global.LevelColor.h * 255.0
+	$Settings/Colors/Snake/SnakeColorSlider.value = Global.SnakeColor.h * 255.0
+	$Settings/Colors/Cherry/CherryColorSlider.value = Global.CherryColor.h * 255.0
+	$Settings/Colors/Ghost/GhostColorSlider.value = Global.GhostColor.h * 255.0
+	$Settings/Colors/Explosion/ExplosionColorSlider.value = Global.ExplosionColor.h * 255.0
+
+func _on_LevelColorSlider_value_changed(value):
+	Global.LevelColor = Color.from_hsv(value / 255.0, 1.0, 1.0)
+	UpdateColors()
+
+func _on_SnakeColorSlider_value_changed(value):
+	Global.SnakeColor = Color.from_hsv(value / 255.0, 1.0, 1.0)
+	UpdateColors()
+
+func _on_CherryColorSlider_value_changed(value):
+	Global.CherryColor = Color.from_hsv(value / 255.0, 1.0, 1.0)
+	UpdateColors()
+
+func _on_GhostColorSlider_value_changed(value):
+	Global.GhostColor = Color.from_hsv(value / 255.0, 1.0, 1.0)
+	UpdateColors()
+
+func _on_ExplosionColorSlider_value_changed(value):
+	Global.ExplosionColor = Color.from_hsv(value / 255.0, 1.0, 1.0)
+	UpdateColors()
+
+func _on_DefaultColorsButton_pressed():
+	Global.LevelColor = Color(1.0, 0.0, 0.471)
+	Global.SnakeColor = Color(0.0, 0.667, 0.043)
+	Global.CherryColor = Color(1.0, 0.0, 0.0)
+	Global.ExplosionColor = Color(0.933, 0.0, 0.0)
+	Global.GhostColor = Color(0.0, 0.882, 1.0)
+	SetColorSliderValues()
